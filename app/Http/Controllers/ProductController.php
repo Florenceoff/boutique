57623b07d2d23dee7e0ef68e820add4d04a11d7c
Florenceoff <fclombe@yahoo.fr>
2024-06-21 10:07:21 +0100

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,12 +6,64 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 
 
-class ProductController extends Controller
+public function index()
 {
-    public function index()
-    {
-        $products = Product::all();
-        // return view('products', compact('products'));
-        return view('products',['product']);
-    }
+    $products = Product::all();
+    return view('products.index', ['products' => $products]);
 }
+
+public function show($id)
+{
+    $product = Product::find($id);
+    return view('products.show', ['product' => $product]);
+}
+
+public function create()
+{
+    return view('products.create');
+}
+
+public function store(Request $request)
+{
+    // Valider les données du formulaire
+    $request->validate([
+        'name' => 'required',
+        'description'=>'required'
+        'price' => 'required',
+    ]);
+
+    // Créer un nouveau produit
+    Product::create($request->all());
+
+    return redirect()->route('products.index');
+}
+
+public function edit($id)
+{
+    $product = Product::find($id);
+    return view('products.edit', ['product' => $product]);
+}
+
+public function update(Request $request, $id)
+{
+    // Valider les données du formulaire
+    $request->validate([
+        'name' => 'required',
+        'description'=> 'required'
+        'price' => 'required',
+    ]);
+
+    // Mettre à jour le produit existant
+    Product::find($id)->update($request->all());
+
+    return redirect()->route('products.index');
+}
+
+public function destroy($id)
+{
+    // Supprimer le produit
+    Product::destroy($id);
+
+    return redirect()->route('products.index');
+}
+
